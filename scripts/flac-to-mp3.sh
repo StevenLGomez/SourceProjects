1c99303a5bf105b4533e5e67031680b96ea9dd6e
Steven Gomez <steve_gomez@usa.net>
2021-06-05 17:06:48 -0500

    Got script working, could use some cleanup though

--- scripts/flac-to-mp3.sh
+++ scripts/flac-to-mp3.sh
@@ -1,3 +1,21 @@
-for f in *.flac; 
-    do flac -cd "$f" | lame -b 320 - "${f%.*}".mp3; 
+
+# flac-to-mp3.sh
+#
+# Requires lame & flac  
+
+# Move MP3 files directly onto USB device
+for f in *.mp3;
+    do
+        echo ''
+        echo Moving $f 
+        mv *.mp3 /media/gomez/BLACKBERRY/
     done
+
+for f in *.flac; 
+    do 
+        echo ''
+        echo Converting $f 
+        flac -cd "$f" | lame -b 320 - /media/gomez/BLACKBERRY/"${f%.*}".mp3; 
+        rm "$f"
+    done
+
