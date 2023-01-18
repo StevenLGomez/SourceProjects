80f7c1ff31a41ded23735fcdfc81738dabadceee
Steven Gomez <steve_gomez@usa.net>
2023-01-17 20:37:51 -0600

    Added flags to music_backup.sh script

--- scripts/music_backup.sh
+++ scripts/music_backup.sh
@@ -1,6 +1,6 @@
 
 # Rsync command to backup SqueezeBox Server to locally attached USB drive.
-# NOTE - There is not a forward slash at the end of the following command, therefore the MusicCollection
+# NOTE - There is not a trailing slash after /media/MusicCollection, therefore the <volume name>/MusicCollection
 #        directory will be used instead of creating it.
-sudo rsync --progress --archive --delete admin@apollo:/media/MusicCollection /media/gomez/<volume name>
+sudo rsync --progress --archive --delete --recursive --compress admin@apollo:/media/MusicCollection /media/gomez/<volume name>
 
