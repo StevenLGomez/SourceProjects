cf768cd028feda85e1f51022380eee4adc4efdfa
Steven Gomez <steve_gomez@usa.net>
2026-01-26 20:30:09 -0600

    Simplified flac-to-mp3 for use with SCARC minutes conversion

--- scripts/flac-to-mp3.sh
+++ scripts/flac-to-mp3.sh
@@ -4,18 +4,19 @@
 # Requires lame & flac  
 
 # Move MP3 files directly onto USB device
-for f in *.mp3;
-    do
-        echo ''
-        echo Moving $f 
-        mv *.mp3 /media/gomez/BLACKBERRY/
-    done
+# for f in *.mp3;
+#     do
+#         echo ''
+#         echo Moving $f 
+#         mv *.mp3 /media/gomez/BLACKBERRY/
+#     done
 
 for f in *.flac; 
     do 
         echo ''
         echo Converting $f 
-        flac -cd "$f" | lame -b 320 - /media/gomez/BLACKBERRY/"${f%.*}".mp3; 
+        # flac -cd "$f" | lame -b 320 - /media/gomez/BLACKBERRY/"${f%.*}".mp3; 
+        flac -cd "$f" | lame -b 320 - ./"${f%.*}".mp3; 
         rm "$f"
     done
 
